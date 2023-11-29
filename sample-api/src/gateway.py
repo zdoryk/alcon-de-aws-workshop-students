c32a82365772f3a3dd0a0b73a4b4e4b589e9e0a5
JakubWalczykowski <127401112+JakubWalczykowski@users.noreply.github.com>
2023-11-29 16:05:20 +0100

    test

--- sample-api/src/gateway.py
+++ sample-api/src/gateway.py
@@ -82,11 +82,37 @@ def get_data(settings: Settings):
                         header=0,
                     )
                 )
+    # if df_list:
+    #     return pd.concat(df_list, ignore_index=True)
+    # return pd.DataFrame()
     if df_list:
-        return pd.concat(df_list, ignore_index=True)
+        
+        df = pd.concat(df_list, ignore_index=True)
+
+        # cleaning code
+        df["DIED"] = df["DATE_DIED"].eq("9999-99-99")
+        df.drop(df[(df["AGE"] < 0) | (df["AGE"] > 110) | (df["AGE"].isnull())].index, inplace=True)
+        df["FULL_NAME"] = df["FIRST_NAME"] + " " + df["LAST_NAME"]
+
+        def idioms(x):
+            if x["AGE"] > 21 and x["SEX"] == "Female":
+                return "Mrs " + x["FULL_NAME"]
+            elif x["AGE"] > 21 and x["SEX"] == "Male":
+                return "Mr " + x["FULL_NAME"]
+            else:
+                return x["FULL_NAME"]
+
+        df["FULL_NAME"] = df.apply(idioms, axis=1)
+
+        max_age = df["AGE"].max()
+        age_bins = list(range(0, max_age + 10, 10))
+        df["AGE_GROUP"] = pd.cut(df["AGE"], bins=age_bins, 
+        labels=[f"{i}-{i+9}" for i in range(0, max_age, 10)], right=False)
+
+        return df
+
     return pd.DataFrame()
-
-
+    
 def lambda_handler(event, context):
     try:
         # Extract query parameters
