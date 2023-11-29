5f0f79e8a7c628bbe1d161b41e1030ab8df73d06
Roman Moskalewski <34585360+skalermo@users.noreply.github.com>
2023-11-29 01:19:37 +0100

    Add lambda_raw implementation

--- src/jobs/lambda_raw.py
+++ src/jobs/lambda_raw.py
@@ -9,13 +9,30 @@ import awswrangler as wr
 logging.getLogger().setLevel(logging.INFO)
 
 
-def get_data_df(hour: str):
+def get_data_df(hour_dt: datetime) -> pd.DataFrame:
     logging.info("Getting data from API")
+
+    api_url = 'https://43bhzz3c3f.execute-api.us-east-1.amazonaws.com/v1/data'
+    headers = {
+        'Authorization': f'Bearer {getenv("AUTH_TOKEN")}'
+    }
+    params = {
+        'date': hour_dt.strftime('%d-%m-%Y'),
+        'start_time': (hour_dt - timedelta(seconds=1)).strftime('%H:%m'),
+        'end_time': hour_dt.strftime('%H:%m'),
+    }
+    response = requests.get(api_url, headers=headers, params=params)
+    return pd.DataFrame(response.json())
 
 
 def main(handler=None, context=None):
     logging.info("Starting lambda_raw job")
 
+    now = datetime.utcnow()
+    df = get_data_df(now.replace(minute=0, second=0))
+
+    filepath = f's3://{getenv("S3_BUCKET_NAME")}/{now.strftime("%d-%m-%Y_%H")}.csv'
+    wr.s3.to_csv(df, path=filepath)
     return {"status": "OK"}
 
 
