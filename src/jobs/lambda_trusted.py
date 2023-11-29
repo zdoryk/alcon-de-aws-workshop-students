28dc832ee6d6052a38f1d05b0ab0385093ab0f35
Roman Moskalewski <34585360+skalermo@users.noreply.github.com>
2023-11-29 12:23:49 +0100

    Add lambda_trusted implementation

--- src/jobs/lambda_trusted.py
+++ src/jobs/lambda_trusted.py
@@ -7,13 +7,31 @@ import logging
 
 logging.getLogger().setLevel(logging.INFO)
 
+AGE = 'AGE'
+
 
 def clean_age_column(df: pd.DataFrame) -> pd.DataFrame:
-    return df
+    logging.info("Cleaning age column")
+    min_years = 0
+    max_years = 100
+
+    cleaned_df = df[df[AGE].notnull()]
+    cleaned_df = cleaned_df[
+        (min_years <= cleaned_df[AGE]) & (cleaned_df[AGE] <= max_years)
+    ]
+    return cleaned_df
 
 
 def main(handler=None, context=None):
-    logging.info("Starting lambda_raw job")
+    logging.info("Starting lambda_trusted job")
+
+    now = datetime.utcnow()
+    raw_path = f's3://{getenv("S3_BUCKET_NAME")}/{now.strftime("%d-%m-%Y_%H")}.csv'
+    df = wr.s3.read_csv(raw_path)
+    cleaned_df = clean_age_column(df)
+
+    trusted_path = f's3://{getenv("S3_BUCKET_NAME")}/trusted/{now.strftime("%d-%m-%Y_%H")}.csv'
+    wr.s3.to_csv(cleaned_df, path=trusted_path, index=False)
 
     return {"status": "OK"}
 
