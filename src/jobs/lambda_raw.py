6442f56ce29f406f875b5f296a513c2c081800ac
Nika Gurchiani <104095410+supernovaedge@users.noreply.github.com>
2023-11-30 15:30:11 +0100

    lambda_raw using awswrangler.

--- src/jobs/lambda_raw.py
+++ src/jobs/lambda_raw.py
@@ -1,18 +1,11 @@
-import os
-import boto3
-from io import StringIO
-from botocore.exceptions import ClientError
 import requests
 from datetime import datetime, timedelta
 import pandas as pd
 import logging
-from io import StringIO
 import awswrangler as wr
 
 logging.basicConfig(level=logging.INFO)
 
-AWS_ACCESS_KEY_ID = 'AKIA23VCC75WKVJ3AF7W'
-AWS_SECRET_ACCESS_KEY = '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'
 BUCKET_NAME = 'alcon-workshop-data-746590502764'
 
 
@@ -44,12 +37,8 @@ def main(event, context):
 
     # End time utc
     end_time_utc = current_utc_datetime
-
-    # Date formats
     date_format = "%d-%m-%Y"
     time_format = "%H:%M"
-
-    # Setting vars
     date = start_time_utc.strftime(date_format)
     start_time = f"{start_time_utc.strftime(time_format)[:2]}:00"
     end_time = f"{end_time_utc.strftime(time_format)[:2]}:00"
@@ -60,19 +49,15 @@ def main(event, context):
     data_response = get_data_df(date, start_time, end_time)
 
     if data_response is not None:
-        # API -> DataFrame
-        df = pd.DataFrame(data_response)
-
-        # Saving file to S3 bucket
         s3_file_path = f"raw/{date}-{start_time[:2]}.csv"
 
         # Dataframe -> CSV
-        csv_buffer = StringIO()
-        df.to_csv(csv_buffer, index=False)
+        wr.s3.to_csv(
+        df=pd.DataFrame(data_response),
+        path=f's3://{BUCKET_NAME}/{s3_file_path}',
+        )
 
-        s3_resource = boto3.resource('s3')
-        s3_resource.Object(BUCKET_NAME, s3_file_path).put(Body=csv_buffer.getvalue())
-
+    logging.info("File Uploaded to S3")
     return {"status": "OK"}
 
 if __name__ == "__main__":
